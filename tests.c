c2cd866fde71ddb145e1e53ddfe95f0cd5d436be
Dimitris Vidos <dvidos@gmail.com>
2024-01-07 13:45:54 -0500

    Some end-to-end tests look promising!

--- tests.c
+++ tests.c
@@ -84,6 +84,7 @@ static void verify_evaluation_iii(char *expression, int a, int b, int expected_r
 
 static void verify_evaluation_ib(char *expression, int a, bool expected_result) {
     dict *values = new_dict(10);
+    dict_set(values, "a", new_int_value(a));
     failable_value evaluation = evaluate(expression, values);
     if (evaluation.failed) { fail(evaluation.err_msg); return; }
     assert_msg(value_as_bool(evaluation.result) == expected_result, expression);
@@ -102,62 +103,63 @@ bool run_unit_tests() {
         - predefined functions: if(cond, true, false), mod(10, 3), left(s, 3)
     */
 
-    // verify_evaluation_null(NULL);
-    // verify_evaluation_null("");
+    verify_evaluation_null(NULL);
+    verify_evaluation_null("");
     // verify_evaluation_null("null");
-    // verify_evaluation_null("1/0"); // error?
 
-    // verify_evaluation_b("false", false);
-    // verify_evaluation_b("true", true);
-    // verify_evaluation_b("!false", true);
-    // verify_evaluation_b("!true", false);
-    // verify_evaluation_b("!(false)", true);
-    // verify_evaluation_b("!(true)", false);
-    // verify_evaluation_b("0", false);
-    // verify_evaluation_b("1", true);
-    // verify_evaluation_b("1234", true);
+    // verify_evaluation_failed("1/0");
 
-    // verify_evaluation_bb("a", true, true);
-    // verify_evaluation_bb("a", false, false);
-    // verify_evaluation_bb("!a", true, false);
-    // verify_evaluation_bb("!a", false, true);
+    verify_evaluation_b("false", false);
+    verify_evaluation_b("true", true);
+    verify_evaluation_b("!false", true);
+    verify_evaluation_b("!true", false);
+    verify_evaluation_b("!(false)", true);
+    verify_evaluation_b("!(true)", false);
+    verify_evaluation_b("0", false);
+    verify_evaluation_b("1", true);
+    verify_evaluation_b("1234", true);
 
-    // verify_evaluation_bbb("a && b", true, true, true);
-    // verify_evaluation_bbb("a && b", true, false, false);
-    // verify_evaluation_bbb("a && b", false, true, false);
-    // verify_evaluation_bbb("a && b", false, false, false);
-    // verify_evaluation_bbb("a || b", true, true, true);
-    // verify_evaluation_bbb("a || b", true, false, true);
-    // verify_evaluation_bbb("a || b", false, true, true);
-    // verify_evaluation_bbb("a || b", false, false, false);
+    verify_evaluation_bb("a", true, true);
+    verify_evaluation_bb("a", false, false);
+    verify_evaluation_bb("!a", true, false);
+    verify_evaluation_bb("!a", false, true);
 
-    // verify_evaluation_i("0", 0);
-    // verify_evaluation_i("1", 1);
-    // verify_evaluation_i("-1", -1);
-    // verify_evaluation_i("1 + 2", 3);
-    // verify_evaluation_i("2 * 3", 6);
-    // verify_evaluation_i("5 - 2", 3);
-    // verify_evaluation_i("8 / 2", 4);
-    // verify_evaluation_i("1 + 2 * 3 + 4", 11);
-    // verify_evaluation_i("1 + (2 * 3) + 4", 11);
-    // verify_evaluation_i("(1 + 2) * (3 + 4)", 21);
+    verify_evaluation_bbb("a && b", true, true, true);
+    verify_evaluation_bbb("a && b", true, false, false);
+    verify_evaluation_bbb("a && b", false, true, false);
+    verify_evaluation_bbb("a && b", false, false, false);
+    verify_evaluation_bbb("a || b", true, true, true);
+    verify_evaluation_bbb("a || b", true, false, true);
+    verify_evaluation_bbb("a || b", false, true, true);
+    verify_evaluation_bbb("a || b", false, false, false);
 
-    // verify_evaluation_ii("a", 4, 4);
-    // verify_evaluation_ii("a + 1", 4, 5);
-    // verify_evaluation_ii("a - 1", 4, 3);
-    // verify_evaluation_ii("a * 2", 4, 8);
-    // verify_evaluation_ii("a / 2", 4, 2);
+    verify_evaluation_i("0", 0);
+    verify_evaluation_i("1", 1);
+    verify_evaluation_i("-1", -1);
+    verify_evaluation_i("1 + 2", 3);
+    verify_evaluation_i("2 * 3", 6);
+    verify_evaluation_i("5 - 2", 3);
+    verify_evaluation_i("8 / 2", 4);
+    verify_evaluation_i("1 + 2 * 3 + 4", 11);
+    verify_evaluation_i("1 + (2 * 3) + 4", 11);
+    verify_evaluation_i("(1 + 2) * (3 + 4)", 21);
 
-    // verify_evaluation_ib("a > 5", 8, true);
-    // verify_evaluation_ib("a > 5", 5, false);
-    // verify_evaluation_ib("a >= 5", 5, true);
-    // verify_evaluation_ib("a < 5", 3, true);
-    // verify_evaluation_ib("a < 5", 5, false);
-    // verify_evaluation_ib("a <= 5", 5, true);
-    // verify_evaluation_ib("a == 5", 5, true);
-    // verify_evaluation_ib("a == 5", 6, false);
-    // verify_evaluation_ib("a != 5", 5, false);
-    // verify_evaluation_ib("a != 5", 6, true);
+    verify_evaluation_ii("a", 4, 4);
+    verify_evaluation_ii("a + 1", 4, 5);
+    verify_evaluation_ii("a - 1", 4, 3);
+    verify_evaluation_ii("a * 2", 4, 8);
+    verify_evaluation_ii("a / 2", 4, 2);
+
+    verify_evaluation_ib("a > 5", 8, true);
+    verify_evaluation_ib("a > 5", 5, false);
+    verify_evaluation_ib("a >= 5", 5, true);
+    verify_evaluation_ib("a < 5", 3, true);
+    verify_evaluation_ib("a < 5", 5, false);
+    verify_evaluation_ib("a <= 5", 5, true);
+    verify_evaluation_ib("a == 5", 5, true);
+    verify_evaluation_ib("a == 5", 6, false);
+    verify_evaluation_ib("a != 5", 5, false);
+    verify_evaluation_ib("a != 5", 6, true);
 
     // verify_evaluation_iii("if(a >  5, b, 1)",  5, 2, 8);
     // verify_evaluation_iii("if(a >= 5, b, 1)", 5, 2, 2);
